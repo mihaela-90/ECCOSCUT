072be2eedbeac95f62d036cec4c6940c7d421269
mihaela-90 <melnicmihaela359@gmail.com>
2026-04-20 22:33:11 +0300

    Add files via upload

---
+++ ecoscut.js
@@ -0,0 +1,20 @@
+
+function openHome() {
+    window.location.href = "ecoscutacasa.html";
+}
+
+function openHarta() {
+    window.location.href = "harta.html";
+}
+
+function openEvenimente() {
+    window.location.href = "evenimente.html";
+}
+
+function openDespreNoi() {
+    window.location.href = "desprenoi.html";
+}
+
+function openConectare() {
+    window.location.href = "conectare.html";
+}
